981f280b56beac626d470605de15003115308d15
steve <steve.tomlin@gmail.com>
2022-06-23 17:46:35 +0100

    add field types for trying mongoimport again via script. Also - fix mongo/export to create write headers, if the export is empty

---
+++ mongodb/README.md
@@ -0,0 +1,10 @@
+# Login interactively
+docker exec -it mongo1 bash
+
+# Login to admin with user/pass
+# User must log into admin database, in order to then switch to database - because we set - "userAdminAnyDatabase" which only applies to admin
+- mongo -u boot -p secret admin
+- use mongo_todos_db
+
+# view imported todos
+db.todos.find().toArray()
--- mongodb/export.js
+++ mongodb/export.js
@@ -10,12 +10,9 @@ const output = `/data/db/${file}`;
 const fields = 'id,todoText,created_timestamp,completed';
 const cmdExport = `docker exec ${MONGO_HOST} mongoexport --authenticationDatabase admin -h ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} -d ${MONGO_DB} -c ${MONGO_COLLECTION}  --type csv --fields ${fields} --out ${output}`;
 
-
 console.log('mongo export...')
 exportCsv({cmdExport, host: MONGO_HOST, output, cb: async () => {
   const pathLocal = path.join(__dirname, `/mongodb_data/${file}`)
-  // onsuccess
-
   const data = await fs.readFileSync(pathLocal, 'utf8')
   if (data.search(/\S/)===-1) {
     fs.writeFile(pathLocal, fields, err => {
