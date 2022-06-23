8043b8ca1107869f7ae5e706fd15f2892a9f900f
steve <steve.tomlin@gmail.com>
2022-06-23 16:56:24 +0100

    export latest.csv for mongo-export with fields if empty

--- export.js
+++ export.js
@@ -1,18 +1,19 @@
 const child_process = require("child_process");
 const { exec } = child_process;
 
-const fnExport = ({cmdExport, host, output}) => {
+const fnExport = ({cmdExport, host, output, cb = () => {}}) => {
   console.log(cmdExport)
   exec(cmdExport, {}, (error, stdout) => {
     if (error) {
       console.error('Cant export csv:', error);
     } else {
       console.log("Export csv success: ", output);
+      cb()
     }
   });
 };
 
-const exportCsv = ({cmdExport, host, output}) => {
+const exportCsv = ({cmdExport, host, output, cb}) => {
   const cmdContainerStopped = `docker container ls -f "status=exited" -f "name=${host}"'`;
   const cmdContainerExist = `docker container ls -f "name=${host}"`
   const cmdRestartContainer = `docker restart ${host}`;
@@ -27,7 +28,7 @@ const exportCsv = ({cmdExport, host, output}) => {
         if (error) {
           console.error('Cant restart container: ', error);
         } else {
-          fnExport({cmdExport, host, output});
+          fnExport({cmdExport, host, output, cb});
         }
       });
     } else {
@@ -40,7 +41,7 @@ const exportCsv = ({cmdExport, host, output}) => {
           Remember next time to run db-export periodically before destroying the container.
           `);
         } else {
-          fnExport({cmdExport, host, output});
+          fnExport({cmdExport, host, output, cb});
         }
       })
     }
--- mongodb/export.js
+++ mongodb/export.js
@@ -1,11 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 const exportCsv = require("../export");
 
 const { env } = process;
 const { MONGO_HOST, MONGO_PORT, MONGO_DB, MONGO_COLLECTION, MONGO_USER, MONGO_PASS} = env;
 
-const output = "/data/db/latest.csv";
-const cmdExport = `docker exec ${MONGO_HOST} mongoexport --authenticationDatabase admin -h ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} -d ${MONGO_DB} -c ${MONGO_COLLECTION}  --type csv --fields id,todoText,created_timestamp,completed --out ${output}`;
+const file = 'latest.csv';
+const output = `/data/db/${file}`;
+const fields = 'id,todoText,created_timestamp,completed';
+const cmdExport = `docker exec ${MONGO_HOST} mongoexport --authenticationDatabase admin -h ${MONGO_HOST} --port ${MONGO_PORT} -u ${MONGO_USER} -p ${MONGO_PASS} -d ${MONGO_DB} -c ${MONGO_COLLECTION}  --type csv --fields ${fields} --out ${output}`;
 
 
 console.log('mongo export...')
-exportCsv({cmdExport, host: MONGO_HOST, output})
+exportCsv({cmdExport, host: MONGO_HOST, output, cb: async () => {
+  const pathLocal = path.join(__dirname, `/mongodb_data/${file}`)
+  // onsuccess
+
+  const data = await fs.readFileSync(pathLocal, 'utf8')
+  if (data.search(/\S/)===-1) {
+    fs.writeFile(pathLocal, fields, err => {
+      if (err) {
+        console.error('error writing to file', err);
+      }
+    });
+  }
+}})
